3bd1f039c23d9b890da052a1f0ec0eb59b304c27
icicel <isakham@chalmers.se>
2023-01-30 18:20:55 +0100

    Separated node enums

--- node.py
+++ node.py
@@ -1,31 +1,4 @@
-class Color:
-    WHITE = 0
-    ORANGE = 1
-    PURPLE = 2
-    PINK = 3
-    CYAN = 4
-    BLACK = 5
-    RED = 6
-    BLUE = 7
-    GREEN = 8
-    BROWN = 9
-    GOLD = 10
-    PURE = 11
-class NodeType:
-    SPACE = -1
-    KEY = 0
-    DOOR = 1
-    DOORNEG = 2
-    KEYABS = 3
-    DOORBLANK = 4
-    DOORX = 5
-    DOORNEGX = 6
-class Effect:
-    FROZEN = 0
-    PAINTED = 1
-    ERODED = 2
-
-
+from node_enums import NodeType, Color, Effect
 
 class Node:
     neighbors: list
---
+++ node_enums.py
@@ -0,0 +1,28 @@
+class Color:
+    WHITE = 0
+    ORANGE = 1
+    PURPLE = 2
+    PINK = 3
+    CYAN = 4
+    BLACK = 5
+    RED = 6
+    BLUE = 7
+    GREEN = 8
+    BROWN = 9
+    GOLD = 10
+    PURE = 11
+class NodeType:
+    SPACE = -1
+    KEY = 0
+    KEYNEG = 1
+    KEYABS = 2
+    DOOR = 3
+    DOORNEG = 4
+    DOORBLANK = 5
+    DOORX = 6
+    DOORNEGX = 7
+class Effect:
+    FROZEN = 0
+    PAINTED = 1
+    ERODED = 2
+    
