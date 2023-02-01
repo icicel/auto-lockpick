255abcf7988dd4a9f5686f53352163f035c88772
icicel <isakham@chalmers.se>
2023-02-01 16:57:45 +0100

    Separated Action

---
+++ action.py
@@ -0,0 +1,26 @@
+from node import Node
+
+class ActionType:
+    OPEN: 0
+    CLEAN: 1
+    CURSE: 2
+    BLESS: 3
+    
+# node - affected node
+# type - OPEN = open (default), CLEAN = remove effect, CURSE = curse, BLESS = remove curse
+class Action:
+    node: Node
+    type: ActionType
+    
+    def __init__(self, node: Node, type: ActionType=ActionType.OPEN) -> None:
+        self.node = node
+        self.type = type
+    
+    def __str__(self) -> str:
+        typeStr = {
+            ActionType.OPEN: "Open",
+            ActionType.CLEAN: "Clean",
+            ActionType.CURSE: "Curse",
+            ActionType.BLESS: "Bless"
+        }[self.type]
+        return f"{typeStr} {self.node.id}"
--- level_state.py
+++ level_state.py
@@ -1,31 +1,6 @@
 from level import Level
 from node import *
-
-class ActionType:
-    OPEN: 0
-    CLEAN: 1
-    CURSE: 2
-    BLESS: 3
-    
-# node - affected node
-# type - OPEN = open (default), CLEAN = remove effect, CURSE = curse, BLESS = remove curse
-class Action:
-    node: Node
-    type: ActionType
-    
-    def __init__(self, node: Node, type: ActionType=ActionType.OPEN) -> None:
-        self.node = node
-        self.type = type
-    
-    def __str__(self) -> str:
-        typeStr = {
-            ActionType.OPEN: "Open",
-            ActionType.CLEAN: "Clean",
-            ActionType.CURSE: "Curse",
-            ActionType.BLESS: "Bless"
-        }[self.type]
-        return f"{typeStr} {self.node.id}"
-
+from action import Action
 
 # keys - amount of keys per color
 # pool - list of nodes "available" in this state
